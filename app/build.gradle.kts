cdc31cf55148d953c85f2e1bffcc3007360fe99b
Alberto Jacini <albertojacini@Albertos-MacBook-Pro.local>
2025-10-02 09:53:05 +0200

    Fixed auth permissions issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        buildConfigField("String", "SUPABASE_URL", "\"http://127.0.0.1:54321\"")
+        buildConfigField("String", "SUPABASE_URL", "\"http://10.0.2.2:54321\"")
         buildConfigField("String", "SUPABASE_ANON_KEY", "\"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0\"")
     }
 
